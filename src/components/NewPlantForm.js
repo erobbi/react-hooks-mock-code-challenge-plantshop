ede31e3030387f1968ceefe589ed5c4f8fab4d8c
erobbi <elirobbins0@gmail.com>
2021-07-28 23:29:44 -0400

    still have more to do

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -43,12 +43,21 @@ function NewPlantForm() {
     })
 })
 
+// adam's way
+
+const makeNewPlant = e => {
+  e.preventDefault()
+  
+}
+
+
   return (
 
     // returing a card at bottom of page
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      {/* add on submit to form */}
+      <form  onSumbit={makeNewPlant}>
         <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={manageFormData} />
         <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={manageFormData}/>
         <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={manageFormData}/>
